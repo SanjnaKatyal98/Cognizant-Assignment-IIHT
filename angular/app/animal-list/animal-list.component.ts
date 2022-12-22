ecde54ffa34f473302e519d409c38e12507e9a3f
Sanjna Katyal <73328935+SanjnaKatyal98@users.noreply.github.com>
2022-12-22 13:05:29 +0530

    Add files via upload

---
+++ angular/app/animal-list/animal-list.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'animal-list',
+  templateUrl: './animal-list.component.html',
+  styleUrls: ['./animal-list.component.css']
+})
+export class AnimalListComponent {
+  showImage:boolean=false;
+  imageWidth:number=150;
+  imageHeight:number=150;
+  imageMargin:number=5;
+  //any data type array
+  arr:any[]=[{
+    name:'lion',
+    imageUrl:'../../assets/roses.jpg'
+  },{
+    name:'dolphin',
+    imageUrl:'../../assets/beach.jpg'
+  },{
+    name:'sparrow',
+    imageUrl:'../../assets/mountain.jpg'
+    }];
+  imageVisibility():void{
+      this.showImage= !this.showImage;
+  }
+}
