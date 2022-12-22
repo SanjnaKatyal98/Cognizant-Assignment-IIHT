4cf28c85e6dd3ef202dd85e603bd77bca59d85f8
Sanjna Katyal <73328935+SanjnaKatyal98@users.noreply.github.com>
2022-12-22 09:51:24 +0530

    Add files via upload

---
+++ 19dec.js
@@ -0,0 +1,10 @@
+var ele = null;
+ele ?? alert(`ele is null`);
+
+let firstname=null;
+let lastname=null;
+let fruit=null;
+
+alert(firstname ?? lastname ?? "anonymous");
+alert(2+4==5||5==5||6==7);
+alert(fruit ?? "Apple");
