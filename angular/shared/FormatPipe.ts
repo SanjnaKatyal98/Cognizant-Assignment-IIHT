b6c53a5f971766332ac33b5baeb6d4ece2ef5080
Sanjna Katyal <73328935+SanjnaKatyal98@users.noreply.github.com>
2023-01-03 11:08:41 +0530

    Delete angular directory

--- angular/shared/FormatPipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from "@angular/core";
-@Pipe({
- name:'formatpipe'
-})
-export class FormatPipe  implements PipeTransform{
-  transform(value: string, num:number):string {
-    return value.repeat(num);
-  }
-}
