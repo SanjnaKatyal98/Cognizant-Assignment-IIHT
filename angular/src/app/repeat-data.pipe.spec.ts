b63943a2e65786b3fff895dd54740246b0e43c1f
Sanjna Katyal <73328935+SanjnaKatyal98@users.noreply.github.com>
2023-01-08 16:02:48 +0530

    Delete angular/src/app directory

--- angular/src/app/repeat-data.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RepeatDataPipe } from './repeat-data.pipe';
-
-describe('RepeatDataPipe', () => {
-  it('create an instance', () => {
-    const pipe = new RepeatDataPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
