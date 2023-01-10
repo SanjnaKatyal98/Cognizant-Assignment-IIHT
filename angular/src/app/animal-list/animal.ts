8756d6180b185d95a3ad62e282c2fd1da06fa85e
Sanjna Katyal <73328935+SanjnaKatyal98@users.noreply.github.com>
2023-01-10 12:45:35 +0530

    Delete angular/src/app directory

--- angular/src/app/animal-list/animal.ts
+++
@@ -1,6 +0,0 @@
-export interface IAnimals{
-    name:string,
-    desc:string,
-    age:number,
-    imageUrl:string
-}
--- angular/src/app/template-forms/get-validators.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GetValidatorsDirective } from './get-validators.directive';
-
-describe('GetValidatorsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new GetValidatorsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
